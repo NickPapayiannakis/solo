c78cd066c553e64613cd44d70388b8c32dca5b50
Nick Papayiannakis <nikos.papayiannakis@gmail.com>
2015-11-11 22:36:13 -0500

    Generated map with position watch and marker.

--- public/app.js
+++ public/app.js
@@ -1,13 +1,12 @@
 // MapBox configuration
 //==========================================
 
-L.mapbox.accessToken = '<your access token here>';
+L.mapbox.accessToken = 'pk.eyJ1Ijoibmlja3BhcGF5aWFubmFraXMiLCJhIjoiY2lndmd6cWY3MHBvbHc3bTVxNWZjemdwcyJ9.KU2_Pibjpd4fuj6YvE76Gg';
 var map = L.mapbox.map('map', 'mapbox.streets')
-    .setView([40, -74.50], 9);
-
+  .setView([40, -74.50], 9);
 
 // Firebase
 //==========================================
 
-var fbase = new Firebase("https://noticeme.heroku.firebaseio.com/");
+//var fbase = new Firebase("https://shining-heat-5760.firebaseapp.com");
 
